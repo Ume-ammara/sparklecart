ef48b60e83ab40f9567c510b2f4d6e8d31b73959
Ume-ammara <amaraume24@gmail.com>
2025-12-01 14:49:46 +0500

    feat: add webhook controller

---
+++ server/src/controllers/webhook.controller.ts
@@ -0,0 +1,3 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Request, Response } from "express";
+export const stripeWebhookController = asyncHandler(async (req: Request, res: Response) => {});
